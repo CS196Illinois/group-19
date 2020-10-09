ffc6a2543b737d7897acba48251ac5a0addd31d9
John <javaldivies@gmail.com>
2020-10-09 04:23:45 -0500

    Fixed CNN bug, cleaned up code

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+
+
 ### Imports ###
 import os
 from selenium import webdriver
@@ -5,24 +7,24 @@ import datetime
 
 ### Initializing Variables ###
 
-cnnFilteredList = []
-cnnLinkList = []
+cnnList = set()
 cnnText = []
-usaFilteredList = []
-usaLinkList = []
+usaList = set()
 usaText = []
-upiFilteredList = []
-upiLinkList = []
+upiList = set()
 upiText = []
+politicoList = set()
+politicoText = []
 cnnSearched = False
 usaSearched = False
 upiSearched = False
-
+politicoSearched = False
 
 ### Finding and setting up chromedriver for webscraping ###
 driver_path = os.path.dirname(os.path.realpath(__file__)) + '\\chromedriver.exe'
+     #PC Driver
+#driver_path = os.path.dirname(os.path.realpath(__file__)) + '/chromedriver'
 browser = webdriver.Chrome(executable_path = driver_path)
-
 
 
 ### Getting Current Date (Used in Webscraping) ###
@@ -43,81 +45,52 @@ def calibrateDate():
         currentDay = '0' + str(dt.day)
     else:
         currentDay = str(dt.day)
-    
 
+    currentDay = "08"
 
-### Webscraping CNN ###
-
+#Webscraping CNN
 def cnnScrape():
-    #Goes to CNN US page and gets links from all headlines
-    browser.get('https://www.cnn.com/us')
+    browser.get('http://lite.cnn.com/en')
     cnnHeadlines = browser.find_elements_by_tag_name('a')
-
-
-    #Checks all links for current date and filters for today
-    targetStart = 'https://www.cnn.com/' + currentYear + '/' + currentMonth + '/' + currentDay
-    for i in range(len(cnnHeadlines)):
+    for i in cnnHeadlines:
         try:
-            cnnLink = cnnHeadlines[i].get_attribute('href')
-            if cnnLink[:len(targetStart)] == targetStart:
-                cnnLinkList.append(cnnLink)
+            if "http://lite.cnn.com/" in i.get_attribute('href'):
+                cnnList.add(i.get_attribute('href'))
         except:
             pass
-
-    #Remove repeat links
-    for i in range(len(cnnLinkList)):
-        if cnnLinkList[i] not in cnnFilteredList:
-            cnnFilteredList.append(cnnLinkList[i])
-
-    #Print list of links
-    print(cnnFilteredList)  
-
-    #Get body paragraph text and add text to cnnText
-    for i in range(len(cnnFilteredList)):
-        articleText = []
-        browser.get(cnnFilteredList[i])
-        articleText = browser.find_elements_by_class_name('zn-body__paragraph')
-        for i in range(len(articleText)):
+    print(cnnList)  
+    for i in cnnList:
+        browser.get(i)
+        articleText = browser.find_elements_by_tag_name('p')
+        for n in articleText:
             try:
-                cnnText.append(articleText[i].text)
-                print(articleText[i].text)
+                if "© 2019 Cable News Network. Turner Broadcasting System, Inc. All Rights Reserved." not in n.text and "Listen to CNN (low-bandwidth usage)" not in n.text and "Go to the full CNN experience" not in n.text:
+                    cnnText.append(n.text)
+                    print(n.text)
             except:
                 pass
-
-    #Make it so storeData() stores CNN data
     global cnnSearched
     cnnSearched = True
 
 
 ### Webscraping USAtoday ###
-
 def usaScrape():
     browser.get('https://www.usatoday.com/news/')
     usaHeadlines = browser.find_elements_by_tag_name('a')
-
-    #Puts all links from today into usaLinkList
-    target = '/' + currentYear + '/' + currentMonth + '/' + currentDay
-    for i in range(len(usaHeadlines)):
+    for i in usaHeadlines:
         try:
-            usaLink = usaHeadlines[i].get_attribute('href')
-            if target in usaLink:
-                usaLinkList.append(usaLink)
+            usaLink = i.get_attribute('href')
+            if '/' + currentYear + '/' + currentMonth + '/' + currentDay + '/' in usaLink:
+                usaList.add(usaLink)
         except:
             pass
-
-    for i in range(len(usaLinkList)):
-        if usaLinkList[i] not in usaFilteredList:
-            usaFilteredList.append(usaLinkList[i])
-    
-    print(usaFilteredList)  
-
-    for i in range(len(usaFilteredList)):
-        articleText = []
-        browser.get(usaFilteredList[i])
+    print(usaList)  
+    for i in usaList:
+        browser.get(i)
         articleText = browser.find_elements_by_class_name('gnt_ar_b_p')
-        for i in range(len(articleText)):
-            usaText.append(articleText[i].text)
-            print(articleText[i].text)
+        for n in articleText:
+            usaText.append(n.text)
+            print(n.text)
     global usaSearched
     usaSearched = True
 
@@ -126,38 +99,49 @@ def usaScrape():
 def upiScrape():
     browser.get('https://www.upi.com/Top_News/US/')
     upiHeadlines = browser.find_elements_by_tag_name('a')
-
-    #Puts all links from today into upiLinkList
-    target = '/' + currentYear + '/' + currentMonth + '/' + currentDay
-    for i in range(len(upiHeadlines)):
+    for i in upiHeadlines:
         try:
-            upiLink = upiHeadlines[i].get_attribute('href')
-            if target in upiLink:
-                upiLinkList.append(upiLink)
+            upiLink = i.get_attribute('href')
+            if '/' + currentYear + '/' + currentMonth + '/' + currentDay in upiLink:
+                upiList.add(upiLink)
         except:
             pass
-
-    for i in range(len(upiLinkList)):
-        if upiLinkList[i] not in upiFilteredList:
-            upiFilteredList.append(upiLinkList[i])
-    
-    print(upiFilteredList)  
-
-    for i in range(len(upiFilteredList)):
-        articleText = []
-        browser.get(upiFilteredList[i])
+    print(upiList)  
+    for i in upiList:
+        browser.get(i)
         articleText = browser.find_elements_by_tag_name('p')
-        for i in range(len(articleText)):
+        for n in articleText:
             try:
-                upiText.append(articleText[i].text)
-                print(articleText[i].text)
+                upiText.append(n.text)
+                print(n.text)
             except:
                 pass
     global upiSearched
     upiSearched = True
 
-
-
+### Webscraping Politico ###
+def politicoScrape():
+    browser.get('https://www.politico.com/')
+    politicoHeadlines = browser.find_elements_by_tag_name('a')
+    for i in politicoHeadlines:
+        try:
+            politicoLink = i.get_attribute('href')
+            if '/' + currentYear + '/' + currentMonth + '/' + currentDay + '/' in politicoLink:
+                politicoList.add(politicoLink)
+        except:
+            pass
+    print(politicoList)
+    for i in politicoList:
+        browser.get(i)
+        articleText = browser.find_elements_by_class_name('story-text__paragraph')
+        for n in articleText:
+            try:
+                politicoText.append(n.text)
+                print(n.text)
+            except:
+                pass
+    global politicoSearched
+    politicoSearched = True
 
 ### Adding scraped data to data.txt ###
 def storeData():
@@ -201,6 +185,17 @@ def storeData():
                 pass
         file.close()
 
+    #Append Politico Data
+
+    if politicoSearched:
+        file = open("data.txt", "a+")
+        for i in range(len(politicoText)):
+            try:
+                file.write(politicoText[i])
+            except:
+                pass
+        file.close()
+
 
 
 ### Main ###
@@ -209,5 +204,6 @@ calibrateDate()
 upiScrape()
 usaScrape()
 cnnScrape()
+politicoScrape()
 storeData()
 print('done')

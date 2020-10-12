6cbce8345f56feb644df3f72187c7f274f9a54dc
xavierc2 <xavierc2@illinois.edu>
2020-10-12 13:36:20 -0500

    add home page

--- Moody/urls.py
+++ Moody/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from dataDisplay import views
 urlpatterns = [
+    path("", views.homeView ),    
     path('admin/', admin.site.urls),
 ]
+

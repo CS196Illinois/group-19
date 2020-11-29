72374ab19af4f0755942514ea52381d19c55cdd7
Nathaniel Smith <ngs.new@gmail.com>
2020-11-28 22:09:49 -0500

    added post model and admin model

--- AIDataModeler/models.py
+++ AIDataModeler/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class SentimentModel(models.Model):
+    positivity = models.FloatField()
+    negativity = models.FloatField()
+
+
+class DaySentimentData(models.Model):
+    date = models.DateField()
+    overall_sentiment = models.ForeignKey(SentimentModel, on_delete=models.DO_NOTHING, related_name='overall')
+    conservative_sentiment = models.ForeignKey(SentimentModel, on_delete=models.DO_NOTHING, related_name='conservative')
+    liberal_sentiment = models.ForeignKey(SentimentModel, on_delete=models.DO_NOTHING, related_name='liberal')
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.title
+
+
+#username: ngs
+#username: djangotime
+

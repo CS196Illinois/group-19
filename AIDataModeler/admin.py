0d947d41a66c8d9cdf19969ed59fa9e8ec1c1ea3
Nathaniel Smith <ngs.new@gmail.com>
2020-11-29 02:02:17 -0500

    made admin page usable

--- AIDataModeler/admin.py
+++ AIDataModeler/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+
+
+class DaySentimentAdmin(admin.ModelAdmin):
+    list_display = ['date']
+    list_filter = ["date"]
+    search_fields = ['date']
+    #prepopulated_fields = {'slug': ('title',)}
+
+
+class SentimentModelAdmin(admin.ModelAdmin):
+    list_display = ['positivity', 'negativity']
+    list_filter = ['positivity', 'negativity']
+    search_fields = ['positivity', 'negativity']
+    #prepopulated_fields = {'slug': ('title',)}
+    
+admin.site.register(DaySentimentData, DaySentimentAdmin)
+admin.site.register(SentimentModel, SentimentModelAdmin)

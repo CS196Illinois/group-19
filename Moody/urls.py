c62dff56dbcbaab94a11aae6e01a7337040c8fc7
Nathaniel Smith <ngs.new@gmail.com>
2020-12-01 18:11:48 -0500

    removed dataDisplay due to redundancy as well as altared base urls for moody root

--- Moody/urls.py
+++ Moody/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
-    path("", include('AIDataModeler.urls')),   
     path('admin/', admin.site.urls),
+    path("", include('AIDataModeler.urls')),   
 ]
 

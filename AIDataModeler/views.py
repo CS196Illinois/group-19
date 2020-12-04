2518acaca749a9bcfb619010a2c7abe9a2b8f698
Nathaniel Smith <ngs.new@gmail.com>
2020-12-03 20:10:15 -0500

    added image display functionality and a page index funcitonality

--- AIDataModeler/views.py
+++ AIDataModeler/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
 
 # Create your views here.
 
 def DayList(request):
-  days = DaySentimentData.objects.all()
+  all_days = DaySentimentData.objects.all()
+
+  paginator = Paginator(all_days, 3)  # 3 posts in each page
+  page = request.GET.get('page')
+  try:
+      days = paginator.page(page)
+  except PageNotAnInteger:
+      days = paginator.page(1)
+  except EmptyPage:
+        days = paginator.page(paginator.num_pages)
+
   context = {
         'days': days
-	}
+  }
   return render(request, 'index.html', context)
+
 
 def DayDetails(request, date_str):
     day = DaySentimentData.objects.get(date_str=date_str)
